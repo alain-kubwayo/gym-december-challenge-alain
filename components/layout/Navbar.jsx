5e765f2938c4ed9da71d6435d517fffcbd23c324
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-04 15:11:15 +0200

    Add mobile breakpoint on Navbar

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Wrapper from "../ui/Wrapper";
 import Link from "next/link";
 
@@ -7,17 +6,16 @@ const Navbar = () => {
         <div className="fixed z-50 w-full bg-primary-200">
             <Wrapper>
                 <nav className="flex items-center justify-between text-white">
-                    <div className="flex gap-x-2">
-                        <Image src="/gallery.svg" width={33} height={33} alt="Gallery Icon" />
-                        <div className="flex py-5 gap-x-2">
-                            <Image src="/tum.svg" width={80} height={42} alt="Gallery Icon" className="pl-1.5" />
+                    <div className="flex items-center gap-x-2">
+                        <img src="/gallery.svg" className="md:w-[33px] md:h-[33px]w-[66px] h-[66px] pr-2.5" alt="Gallery Icon"/>
+                        <div className="flex py-8 gap-x-2">
+                            <img src="/tum.svg" alt="Gallery Icon" className="w-[160px] h-[84px] md:w-[80px] md:h-[42px]" />
                             <p className="hidden text-xs xl:block">
                                 Technical<br />
                                 University<br />
                                 of Munich
                             </p>
                         </div>
-                        
                     </div>  
                     <div className="flex items-center">
                         <ul className="hidden xl:flex items-center gap-x-6  mr-[54px] font-bold">
@@ -43,16 +41,16 @@ const Navbar = () => {
                                 <Link href="#">about tum</Link>
                             </li>
                         </ul>
-                        <div className="flex -mt-[11px] gap-x-2">
-                            <div className="flex gap-x-2">
+                        <div className="flex -mt-[11px] gap-x-6 md:gap-x-2 items-center">
+                            <div className="flex items-center text-4xl md:text-base gap-x-3 md:gap-x-2">
                                 <Link href="#" className="text-gray-500">DE</Link>
-                                <Link href="#" className="pl-2 border-l border-white solid">EN</Link>
+                                <Link href="#" className="pl-3 pr-2 border-l-4 border-white md:border-l md:pl-2 solid">EN</Link>
                             </div>
-                            <div className="px-5">
-                                <Image src="/search.svg" width={24} height={24} alt="Search Icon" />
+                            <div className="p-2.5 md:px-5">
+                                <img src="/search.svg" className="w-[48px] h-[48px] md:w-[24px] md:h-[24px]" alt="Search Icon" />
                             </div>
-                            <div className="block xl:hidden">
-                            <Image src="/hamburger.svg" width={24} height={24} alt="Search Icon" />
+                            <div className="p-2.5 md:p-0 block xl:hidden">
+                                <img src="/hamburger.svg" className="w-[48px] h-[48px] md:w-[24px] md:h-[24px]" alt="Search Icon" />
                             </div>
                         </div>
                     </div>
