4274a8b153fcdece6cf31d0c7aee8be2da0c3dc3
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-04 16:51:50 +0200

    add mobile breakpoint to President section

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
       {/* <Hero />
       <News />
       <Discover />
-      <Degree />
-      <President /> */}
+      <Degree /> */}
+      <President />
       <Statistics />
       <Schools />
       <Alumni />
