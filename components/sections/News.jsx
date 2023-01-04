62ef4e75ee91bd96d1466510f14f336df48fce46
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-04 17:21:42 +0200

    add mobile breakpoint to News section

--- components/sections/News.jsx
+++ components/sections/News.jsx
@@ -8,20 +8,25 @@ const News = () => {
     return ( 
         <Wrapper>
             <div>
-                <div className="grid grid-cols-2 xl:mt-12 gap-x-12">
-                    <div className="space-y-6">
+                <div className="block mb-6 space-y-6 md:hidden">
+                    <h1 className="text-8xl">News from TUM</h1>
+                    <p className="text-xl">News, research results and events: everything that enthuses people at our university.</p>
+                    <button className="px-5 py-4 text-base text-white uppercase bg-primary-300">See All</button>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:mt-12 md:gap-x-12">
+                    <div className="hidden space-y-6 md:block">
                         <h1 className="text-8xl">News from TUM</h1>
                         <p className="text-xl">News, research results and events: everything that enthuses people at our university.</p>
                         <button className="px-5 py-4 text-base text-white uppercase bg-primary-300">See All</button>
                     </div>
-                    <div className="grid grid-cols-2 gap-6">
+                    <div className="grid grid-cols-1 md:gap-6 md:grid-cols-2">
                         {newsOne.map((news, id) => <NewsCard news={news} key={id} />)}
                     </div>
-                    <div className="grid grid-cols-2 col-span-2 gap-6 w-full xl:w-[696px] relative left-[40%] mt-12">
+                    <div className="grid grid-cols-1 md:grid-cols-2 col-span-2 gap-6 w-full xl:w-[696px] md:relative md:left-[40%] mt-12">
                         {newsTwo.map((news, id) => <NewsCard news={news} key={id} />)}
                     </div>
                 </div>
-                <div>
+                <div className="hidden md:block">
                 </div>
             </div>
         </Wrapper>
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <Hero />
-      <News /> */}
+       */}
+      <News />
       <Discover />
       <Degree />
       <President />
